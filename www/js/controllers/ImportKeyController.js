d3be1b6edff644cb34bde48a20ad0194a7082735
Kristo Kaarmann <kristo.kaarmann@gmail.com>
2016-10-20 23:00:39 +0100

    Added import account

--- www/js/controllers/ImportKeyController.js
+++ www/js/controllers/ImportKeyController.js
@@ -1,17 +1,27 @@
 export default class ImportKeyController {
 
-    constructor($scope, $state, sdk) {
+    constructor($scope, $state, sdk, $timeout) {
         $scope.$watch(function () {
             if (!sdk.isUnlocked() && $state.current.name != 'main') {
                 $state.go('main');
             }
         });
+        $scope.changePwd = () => {
+                $scope.wrongpwd = 0;
+        }
+	$scope.key = {};
         $scope.importKey = () => {
-            console.log("Import key. NOT IMPLEMENTED.");
+            console.log("Import key.", $scope.key);
+            if ($scope.key.password && sdk.unlock($scope.key.password)) {
+                $scope.newaddr = '0x'+(sdk.pubToAddress(sdk.storeNewKey($scope.key.privKey))).toString('hex');
+                $timeout(() => { $state.go('navBar.transactions') },1000);
+            } else {
+                $scope.wrongpwd = "true";
+            }
         }
 
     }
 
 }
 
-ImportKeyController.$inject = ['$scope', '$state', 'sdk'];
+ImportKeyController.$inject = ['$scope', '$state', 'sdk','$timeout'];
