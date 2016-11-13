dd966270c4ecf0f9b54439c7d247cb769ebbf844
Marat Ismagilov <marat.ismagilov@transferwise.com>
2016-11-13 21:41:42 +0200

    fix transactions state initiating problem with nasty hack

--- www/js/controllers/TransactionsController.js
+++ www/js/controllers/TransactionsController.js
@@ -3,35 +3,43 @@ import moment from 'moment/moment';
 export default class TransactionsController {
 
     constructor($scope, $state, sdk) {
-        $scope.$watch(function () {
+        this.$scope = $scope;
+        this.sdk = sdk;
+
+        $scope.$watch(() =>{
             if (!sdk.isUnlocked() && $state.current.name != 'main') {
                 $state.go('main');
+                return;
+            }
+
+            if (sdk.isUnlocked() && !this.$scope.idCode) {
+                this.loadData();
             }
         });
-	this.$scope = $scope;
-	this.sdk = sdk;
-	this.loadData();
-	$scope.refreshClick = () => { this.loadData() };
+
+        $scope.refreshClick = () => {
+            this.loadData()
+        };
     }
 
     loadData() {
-	console.log("refreshing tx screen");
-	this.$scope.refreshing = true;
-        this.$scope.idCode =this.sdk.getEstonianIdCode();
-        this.sdk.balanceTotalAsync().then( (amount) => { 
-		this.$scope.totalBalance = amount; 
-		this.$scope.$apply();
-	})
-        this.$scope.addresses = this.sdk.addresses()
+        console.log("refreshing tx screen");
+        this.$scope.refreshing = true;
+        this.$scope.idCode = this.sdk.getEstonianIdCode();
+        this.sdk.balanceTotalAsync().then((amount) => {
+            this.$scope.totalBalance = amount;
+            this.$scope.$apply();
+        });
+        this.$scope.addresses = this.sdk.addresses();
 
-        this.sdk.transfersCleanedAsync().then( (tx) => {
-		this.$scope.transfers = tx; 
-	        this.$scope.refreshing = false;
-		this.$scope.$apply();
-	})
+        this.sdk.transfersCleanedAsync().then((tx) => {
+            this.$scope.transfers = tx;
+            this.$scope.refreshing = false;
+            this.$scope.$apply();
+        })
     }
 
-    getFormattedDate(tx){
+    getFormattedDate(tx) {
         return moment(tx.timestamp).fromNow();
     }
 
