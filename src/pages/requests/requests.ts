0f16b51b5044f23a5777496f9db5b306bafff5e2
rostog <rostogiorgi@gmail.com>
2017-09-10 00:05:30 +0300

    ui fixes to send request

--- src/pages/requests/requests.ts
+++ src/pages/requests/requests.ts
@@ -17,34 +17,20 @@ import moment from 'moment';
 
 @Component({selector: 'page-requests', templateUrl: 'requests.html'})
 export class RequestsPage {
-
-  SPRAYER_ADDRESS = "0xa5f1eea6d0a14c8e37cad8019f67b9ca19768f55" // CHANGE ME
-  owner: { firstName?: string, lastName?: string } = {};F
   idCode: string;
-  totalBalance: number;
-  totalPending: number;
   enableRemoveButtonTime: number = Date.now() - 600000; // allow stop watching after 10min
-  transfers: Transfer[] = [];
-  pendingTransfers: Transfer[] = [];
+  requests: Transfer[] = []; // change to Request[]
   refreshing: boolean = false;
-  sprayer: {dismissed : boolean, hasOutgoing : boolean, hasIncome : boolean} = { dismissed : false, hasOutgoing : false, hasIncome : false}; 
 
   constructor(public navCtrl: NavController,
               private toastCtrl: ToastController,
               public events: Events,
               public sdk: SdkService,
-	      public push: Push,
-	      public  plt: Platform,
+	            public push: Push,
+	            public  plt: Platform,
               private alertCtrl: AlertController) {
     this.idCode = this.sdk.getEstonianIdCode();
-    this.sprayer.dismissed = this.sdk.loadSprayerDismissed();
     this.loadData(null);
-    this.sdk.nameFromIdAsync(this.idCode).then((nameJson) => {
-      this.owner = nameJson;
-    });
-    events.subscribe('tx:newPending', () => this.refreshPending());
-
-
 
     // subscribe  to push notifications every time
     if (plt.is('cordova')) {
@@ -77,41 +63,8 @@ export class RequestsPage {
 
   loadData(refresher) {
     this.refreshing = true;
-    this.sdk.balanceTotalAsync().then((amount) => {
-      this.totalBalance = amount;
-    });
-    this.refreshPending();
 
-    this.transfers = [];
-    this.sdk.transfersCleanedAsync().then((tx) => {
-      if (tx) {
-        this.transfers = tx.sort(function (a, b) {
-          return a.timestamp - b.timestamp;
-        }).reverse();
-	this.refreshSprayer();
-      }
-      this.refreshing = false;
-      if (refresher) {
-        refresher.complete();
-      }
-    })
-  }
-
-  refreshSprayer() {
-    if (!this.sprayer.dismissed) {
-      // check if hasIncome
-	if (this.transfers.filter( (tx) => 
-		tx.sourceAccount.toLowerCase().indexOf(this.SPRAYER_ADDRESS.toLowerCase()) > -1
-	).length) {
-		this.sprayer.hasIncome = true;
-	};
-      // check if any outgoing tx
-	if (this.transfers.filter( (tx) => 
-		tx.signedAmount < 0
-	).length) {
-		this.sprayer.hasOutgoing = true;
-	};
-    }
+    this.requests = [];
   }
 
   getLongDate(tx: Transfer): string {
@@ -126,15 +79,6 @@ export class RequestsPage {
     this.navCtrl.push(TopupPage);
   }
 
-  toSprayer() {
-    this.navCtrl.push(SprayerPage);
-  }
-
-  sprayerDismiss() {
-    this.sprayer.dismissed = true;
-    this.sdk.saveSprayerDismissed();
-  }
-
   toSendPage() {
     this.navCtrl.push(SendRequestPage);
   }
@@ -147,52 +91,4 @@ export class RequestsPage {
     return !!this.sdk.isUnlocked();
   }
 
-  private refreshPending() {
-    this.enableRemoveButtonTime = Date.now() - 600000;
-    this.pendingTransfers = this.sdk.getPendingTransfers();
-    this.totalPending = this.sdk.getPendingTotal();
-    if (!this.pendingTransfers) {
-      return
-    }
-    console.log("starting to refresh  pending for amount: ", this.totalPending);
-
-    let pendingCheck = Observable.interval(10000).take(25);
-    let checkAction = pendingCheck.subscribe(
-      (x) => {
-        if (this.pendingTransfers == null || this.pendingTransfers.length == 0) {
-          checkAction.unsubscribe();
-          pendingCheck = undefined;
-        }
-        console.log("Pending refresh TX try: ", x);
-        this.pendingTransfers.map((pendingTx) => {
-          pendingTx.pendingRefresh = true;
-          this.sdk.transferStatusAsync(pendingTx.transactionHash).then((txCheckStatus: string) => {
-
-            if (txCheckStatus != "PENDING") {
-              this.sdk.removePendingTransfer(pendingTx.transactionHash);
-              this.toastCtrl.create({message: 'Confirmed ' + pendingTx.transactionHash, duration: 5000});
-              this.loadData(null);
-            } else {
-            }
-            this.pendingTransfers = this.sdk.getPendingTransfers();
-            this.totalPending = this.sdk.getPendingTotal();
-            setTimeout(() => {
-              pendingTx.pendingRefresh = false;
-            }, 500);
-          });
-        });
-      });
-  };
-
-  removePending(txhash: string): void {
-    this.sdk.removePendingTransfer(txhash);
-    this.refreshPending();
-  }
-
-  testit() {
-    var arr = [{amount: 32, transactionHash: '0x223'}, {amount: 33, transactionHash: '0x444'}];
-    this.sdk.escrowToPending(arr);
-    this.refreshPending()
-
-  }
 }
